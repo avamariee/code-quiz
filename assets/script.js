037453e30c56eeaf4721201b2671213912afd9fc
Ava Bullock <ava.m.bullock@gmail.com>
2021-02-14 13:53:55 -0700

    commented out more steps, fixed the start button so the eventlistener works and displays text when clicked on.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,10 @@
 // variable to select the start button
 var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
+var questionContainerEl = document.getElementById("question-container");
+
+// eventListener to start the quiz when the button is pushed
+startButton.addEventListener("click", startGame);
 
 //variable to select body for DOM manipulation
 
@@ -11,15 +16,60 @@ var scoreTracker = 0;
 // quiz questions array
 var questions = [
 
-    { Q: "Which of the following data type uses true/false logic?", choices: ["String.", "Number.", "Array.", "Boolean."], answer: "Boolean." },
-    { Q: "What do you use to print data in the console?", choices: ["addEventListener.", "console.log()", "getElementById", "Math.floor"], answer: "console.log" },
-    { Q: "What word do you use to define a variable?", choices: ["var", "let", "const", "all of the above."], answer: "all of the above." },
-    { Q: "How do you store data in the user's browser?", choices: ["console.log", "localStorage.setIem", "localStorage.getItem", "storeInfo"], answer: "localStorage.setItem" }
+    {
+        Q: "Which of the following data type uses true/false logic?",
+        choices: ["String.", "Number.", "Array.", "Boolean."],
+        answer: "Boolean."
+    },
+    {
+        Q: "What do you use to print data in the console?",
+        choices: ["addEventListener.", "console.log()", "getElementById", "Math.floor"],
+        answer: "console.log"
+    },
+    {
+        Q: "What word do you use to define a variable?",
+        choices: ["var", "let", "const", "all of the above."],
+        answer: "all of the above."
+    },
+    {
+        Q: "How do you store data in the user's browser?",
+        choices: ["console.log", "localStorage.setIem", "localStorage.getItem", "storeInfo"],
+        answer: "localStorage.setItem"
+    }
 ]
 
 // variable to keep track of time left
 var timeLeft = 75;
 
+// function to start the game
+function startGame() {
+
+    //code to start game here
+    console.log("It works!")
+    displayQuestion()
+
+    // add code to hide start button after pressing it
+    // add code to display question container element
+
+
+}
+
+// function to display each question after the start button is pressed
+
+function displayQuestion() {
+
+    // code to display questions
+    
+    // code to display choices
+
+}
+
+// function to pick the correct answer in relation to the displayed question
+
+function questionAnswer() {
+
+
+}
 
 // for loop to iterate over each question in the 'questions' array
 for (let i = 0; i < questions.length; i++) {
@@ -35,40 +85,10 @@ for (let i = 0; i < questions.length; i++) {
     }
 }
 
-// function to display questions
-
-function displayQuestion() {
-
-    // code to display questions
-    var quizQuestion = document.createElement("h2")
-    var questionTitle = document.textContent(question.Q);
-    quizQuestion.appendChild(questionTitle);
-    document.body.appendChild(quizQuestion);
-    // code to display choices
-
-}
-
-// eventListener to start the quiz when the button is pushed
-startButton.addEventListener("click", function () {
-    if (questions < 4 && timeLeft > 0) {
-
-        displayQuestion();
-
-    } else {
-        
-        // stop showing questions and display final score
-
-    }
-})
-
 // when game ends display final score and have user input their intials to save score in local storage
 
 scoreTracker = timeLeft;
 
 // display high scores
-
-// user clicks button to start game
-startButton.onlick = displayQuestion;
-
 
 
