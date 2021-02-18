888dfd2b01997ca70a4d008dd65d16bf534d7261
Ava Bullock <ava.m.bullock@gmail.com>
2021-02-17 20:50:09 -0700

    reworked array and made it so choices button appear and everything else is hidden when button is clicked

--- assets/script.js
+++ assets/script.js
@@ -1,89 +1,102 @@
 // variable to select the start button
 var startButton = document.getElementById("start-btn");
+var startText = document.getElementById("start-text");
 var nextButton = document.getElementById("next-btn");
 var questionContainerEl = document.getElementById("question-container");
+var questionEl = document.getElementById("question")
+var choiceButtonEl = document.getElementById("choices")
+
+
+
+// variable to keep track of time left
+var timeLeft = 75;
+
+// variable to keep track of the user's score
+var scoreTracker = 0;
+
+
+// function to start the game
+function startGame() {
+
+    //code to start game here
+    console.log("It works!");
+    // add code to hide start button & start text after pressing it
+    startButton.classList.add("hide");
+    startText.classList.add("hide");
+    // add code to display question container element
+    questionContainerEl.classList.remove("hide");
+
+    displayQuestion()
+
+}
 
 // eventListener to start the quiz when the button is pushed
 startButton.addEventListener("click", startGame);
 
-//variable to select body for DOM manipulation
-
-var body = document.body;
-
-// variable to keep track of the user's score
-var scoreTracker = 0;
 
 // quiz questions array
 var questions = [
 
     {
         Q: "Which of the following data type uses true/false logic?",
-        choices: ["String.", "Number.", "Array.", "Boolean."],
-        answer: "Boolean."
+        choices: [
+
+            { choice: "String.", correct: false },
+            { choice: "Number.", correct: false },
+            { choice: "Array.", correct: false },
+            { choice: "Boolean.", correct: true }
+        ]
     },
     {
         Q: "What do you use to print data in the console?",
-        choices: ["addEventListener.", "console.log()", "getElementById", "Math.floor"],
-        answer: "console.log"
+        choices: [
+
+            { choice: "addEventListener.", correct: false },
+            { choice: "console.log()", correct: true },
+            { choice: "getElementById", correct: false },
+            { choice: "Math.floor", correct: false }
+        ]
     },
     {
         Q: "What word do you use to define a variable?",
-        choices: ["var", "let", "const", "all of the above."],
-        answer: "all of the above."
+        choices: [
+
+            { choice: "var", correct: false },
+            { choice: "let", correct: false },
+            { choice: "const", correct: false },
+            { choice: "All of the above.", correct: true }
+        ]
     },
     {
         Q: "How do you store data in the user's browser?",
-        choices: ["console.log", "localStorage.setIem", "localStorage.getItem", "storeInfo"],
-        answer: "localStorage.setItem"
+        choices: [
+
+            { choice: "console.log", correct: false },
+            { choice: "localStorage.setIem", correct: true },
+            { choice: "localStorage.getItem", correct: false },
+            { choice: "storeInfo", correct: false }
+        ]
     }
 ]
 
-// variable to keep track of time left
-var timeLeft = 75;
 
-// function to start the game
-function startGame() {
-
-    //code to start game here
-    console.log("It works!")
-    displayQuestion()
-
-    // add code to hide start button after pressing it
-    // add code to display question container element
-
-
-}
 
 // function to display each question after the start button is pressed
 
 function displayQuestion() {
 
     // code to display questions
-    
-    // code to display choices
 
 }
 
 // function to pick the correct answer in relation to the displayed question
 
-function questionAnswer() {
+function questionChoices() {
 
+  // code to display choices
 
 }
 
-// for loop to iterate over each question in the 'questions' array
-for (let i = 0; i < questions.length; i++) {
-    var question = questions[i];
-    var answer = question.Q;
-    // check if user's answer is correct
-    if (answer === question.answer) {
-        // diplay "correct" underneath quiz box
-
-    } else {
-        // display "incorrect" underneath quiz box
-        timeLeft - 10
-    }
-}
 
 // when game ends display final score and have user input their intials to save score in local storage
 
