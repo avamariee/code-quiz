5a5ddb8efa89c82a82ac7c62abbc9bb2b04c8def
Ava Bullock <ava.m.bullock@gmail.com>
2021-02-20 15:50:54 -0700

    added for loop to loop over questions and display questions

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,32 @@
+// variable to select the initial screen's text
+var startText = document.getElementById("start-text");
+
 // variable to select the start button
 var startButton = document.getElementById("start-btn");
-var startText = document.getElementById("start-text");
+
+// variable to select the next button
 var nextButton = document.getElementById("next-btn");
+
+// variable to select the question container div
 var questionContainerEl = document.getElementById("question-container");
+
+// variable to select the question div
 var questionEl = document.getElementById("question")
-var choiceButtonEl = document.getElementById("choices")
 
+// variable to select the choices div
+var choiceEl = document.getElementById("choices")
 
+// variable to select the choice buttons
+var choiceButtonEl = document.getElementById("choice-btn")
+
+// variable to shuffle questions
+let shuffledQuestions, currentQuestionIndex
 
 // variable to keep track of time left
 var timeLeft = 75;
 
 // variable to keep track of the user's score
 var scoreTracker = 0;
-
-
-// function to start the game
-function startGame() {
-
-    //code to start game here
-    console.log("It works!");
-    // add code to hide start button & start text after pressing it
-    startButton.classList.add("hide");
-    startText.classList.add("hide");
-    // add code to display question container element
-    questionContainerEl.classList.remove("hide");
-
-    displayQuestion()
-
-}
-
-// eventListener to start the quiz when the button is pushed
-startButton.addEventListener("click", startGame);
-
 
 // quiz questions array
 var questions = [
@@ -79,13 +73,41 @@ var questions = [
     }
 ]
 
+console.log(questions[0].choices[1].choice)
+// function to start the quiz
+function startQuiz() {
 
+    console.log("It works!");
+    // add code to hide start button & start text after pressing it
+    startButton.classList.add("hide");
+    startText.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    // add code to display question container element
+    questionContainerEl.classList.remove("hide");
+    displayNextQuestion();
+    
+
+
+}
 
 // function to display each question after the start button is pressed
 
-function displayQuestion() {
+function displayNextQuestion() {
 
-    // code to display questions
+    console.log(shuffledQuestions,currentQuestionIndex)
+
+    displayQuestion(shuffledQuestions[currentQuestionIndex])
+    questionChoices(questions.choices)
+   
+
+
+}
+
+function displayQuestion(questions) {
+
+    questionEl.innerText = questions.Q
+
 
 }
 
@@ -93,14 +115,118 @@ function displayQuestion() {
 
 function questionChoices() {
 
-  // code to display choices
+    // check if currentQuestionIndex > length of the questions (stop condition), if greater than, stop the timer/clock. 
+    if (currentQuestionIndex > questions.length) {
+
+        endQuiz();
+
+    } else {
+
+        document.getElementById("choices").innerText = ""
+
+        // create for loop to go over choices
+
+        for (let j = 0; j < 4; j++) {
+
+            // console.log(questions[0].choices[j].choice)
+            // console.log(j)
+
+            // create a button with event listener, append it to id of "choices"
+            const choiceBtn = document.createElement("button")
+            choiceBtn.classList.add("choice-btn")
+            choiceBtn.setAttribute("id", j)
+            // choiceBtn.setAttribute("id", questions[currentQuestionIndex].choices[j].choice)
+            choiceBtn.innerText = questions[currentQuestionIndex].choices[j].choice
+            choiceBtn.addEventListener("click", function () {
+
+
+                var questionAnswer = (questions[currentQuestionIndex].choices[choiceBtn.id].correct)
+
+                console.log(currentQuestionIndex)
+
+                if (questionAnswer === true) {
+
+                    alert("Correct!")
+
+                    
+                    
+
+
+
+
+
+
+                } else {
+
+                    alert("Incorrect!")
+
+
+
+
+
+
+                }
+
+                setTimeout(function (){
+
+                    displayNextQuestion(currentQuestionIndex++);
+
+
+                }, 1000)
+                
+
+
+            })
+            document.getElementById("choices").appendChild(choiceBtn)
+
+            // // on click event for the buttons, true/false logic, add currentQuestionIndex++, call questionChoices() again from function 
+        }
+
+
+
+
+
+    }
+
+    // const body = document.querySelector("body")
+
+
+    // body.addEventListener("click", function (e) {
+
+    //     console.log(e.target.id)
+
+
+    // })
 
 }
 
 
-// when game ends display final score and have user input their intials to save score in local storage
 
-scoreTracker = timeLeft;
+
+
+// function to end the quiz
+
+function endQuiz() {
+
+
+
+
+}
+
+
+// function to display user's score
+
+function displayScores() {
+
+    // when game ends display final score and have user input their intials to save score in local storage
+
+    scoreTracker = timeLeft;
+}
+
+
+// eventListener to start the quiz when the button is pushed
+startButton.addEventListener("click", startQuiz);
+
 
 // display high scores
 
